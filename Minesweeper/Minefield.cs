ecb3c5f1ad6372dbea7a3495772d825dcbd35383
Filip Tran <filipletran@gmail.com>
2023-08-08 20:53:49 +0200

    fix endless loop bug

--- Minesweeper/Minefield.cs
+++ Minesweeper/Minefield.cs
@@ -26,6 +26,8 @@ class Minefield
     public int[] revealSquares(string guess)
     {
         int[] coordinates = parseCoordinates(guess);
+
+
         if(isMine(coordinates[0], coordinates[1])) // dead 
         {
             return null;
@@ -40,57 +42,69 @@ class Minefield
     {
         int[] adjacentSquares = new int[9];
         int index = 0;
-        for(int x = xCord-1; x < x+2; x++)
+        for(int x = xCord-1; x < xCord+2; x++)
         {
-            for(int y = yCord+1; y > y-2; y--)
+            for(int y = yCord+1; y > yCord-2; y--)
             {
-                adjacentSquares[index] = getNeighbours(x, y);
-                index++;
+                if(isMine(x, y))
+                {
+                    adjacentSquares[index] = -1;    
+                }
+                else 
+                {
+                    adjacentSquares[index] = getNeighbours(x, y);
+                    index++;
+                }
             }
         }
-        
-        //      2 1
-        // 1 2, 2 2, 3 2
-        // 1 1, o G, 3 1
-        // 1 0, 2 0, 3 0
-
         return adjacentSquares;
     }
 
     private int getNeighbours(int xCord, int yCord)
     {
         int counter = 0;
-        for(int x = xCord-1; x < x+2; x++)
+        for(int x = xCord-1; x < xCord+2; x++)
         {
-            for(int y = yCord+1; y > y-2; y--)
+            for(int y = yCord+1; y > yCord-2; y--)
             {
-                if(x < 0 || x > xLength || y < 0 || y > yLength) continue;
+                if(isOutsideField(x, y)) continue;
                 if(isMine(x, y)) counter++;
             }
         }
         return counter;
     }
 
-    private int[] parseCoordinates(string guess)
+    public int[] parseCoordinates(string guess)
     {
-        if(guess.Length != 3) 
-        {
-            throw new InvalidOperationException("Please type the in the following format: 'x y'");
-        } 
-        else 
-        {
-            string[] coordinates = guess.Split(' ');
-            return new int[2] {stringToInt(coordinates[0]), stringToInt(coordinates[1])};
-        }
+        string[] coordinates = guess.Split(' ');
+        return new int[2] {stringToInt(coordinates[0]), stringToInt(coordinates[1])};
     }
-
     private int stringToInt(string str) 
+
     {
         return Int32.Parse(str);
     }
 
+    private bool isOutsideField(int x, int y)
+    {
+        return x < 0 || y < 0 || x > xLength || y > yLength;
+    }
+
     private bool isMine(int x, int y)
     {
-        return _bombLocations[x, y];
+        return checkMineLocation(x, y);
+    }
+
+    private bool checkMineLocation(int x, int y)
+    {
+        if(isOutsideField(x, y))
+        {
+            Console.WriteLine("Outside field range!");
+            return false;
+        } 
+        else 
+        {
+            return _bombLocations[x, y];
+        }
     }
 }
