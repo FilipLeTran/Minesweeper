f60bdf6794bab2d031b0f49b7825a5861dd8f149
Filip Tran <filipletran@gmail.com>
2023-08-08 20:54:29 +0200

    add updateBoard for selected input

--- Minesweeper/MineView.cs
+++ Minesweeper/MineView.cs
@@ -18,6 +18,34 @@ class MineView
         Console.WriteLine(printYField(this.field));
     }
 
+    public void updateBoard(int[] squares, int[] coordinates)
+    {
+        Console.WriteLine(printXField(this.field));
+        StringBuilder sb = new StringBuilder("", 50);
+        int[] coords = coordinates;
+        int index = 0;
+        for(int i = field.getSize()[1]-1; i >= 0; i--)
+        {
+            sb.Append(i);
+            for(int k = 0; k < field.getSize()[0]; k++)
+            {
+                if(coords[0] == i && coords[1] == k)
+                {
+                    if(squares[index] == -1) sb.Append("x");
+                    sb.Append(squares[index].ToString());
+                    index++;
+                }
+                else
+                {
+                    sb.Append("?");
+                }
+            }
+            sb.Append("\n");
+        }
+        sb.Append("\n");
+        Console.Write(sb);
+    }
+
     private StringBuilder printXField(Minefield field) 
     {
         StringBuilder sb = new StringBuilder(" ", 50);
